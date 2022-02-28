c8cb515ac2a32f2150b1f3d1506aeb4ebb519a38
joesphchang <joesphschang@gmail.com>
2022-02-28 14:05:54 -0500

    Finished assessement

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  resources :posts
+  
+  namespace :api, defaults: {format: :json} do
+    resources :posts
+  end
+end
---
+++ db/migrate/20220228190142_create_posts.rb
@@ -0,0 +1,14 @@
+class CreatePosts < ActiveRecord::Migration[7.0]
+  def change
+    create_table :posts do |t|
+      t.string :author
+      t.integer :authorId
+      t.integer :likes
+      t.integer :popularity
+      t.integer :reads
+      t.text :tags
+
+      t.timestamps
+    end
+  end
+end
